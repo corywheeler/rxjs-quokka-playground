8f4c96c5201331128debe4dfa6b42dc3938c4a27
Cory Wheeler <corywheeler@gmail.com>
2020-04-17 13:12:09 -0400

    Wire in something to test RXjs

--- test.js
+++ test.js
@@ -1,3 +1,14 @@
+import { fromEvent } from 'rxjs';
+import { scan } from 'rxjs/operators';
+
 const testDiv = document.getElementById('testDiv');
 
 testDiv.textContent
+
+fromEvent(document, 'click')
+	.pipe(scan(count => count + 1, 0))
+	.subscribe(count => console.log(`Clicked ${count} times`));
+
+let clickEvent = new window.Event('click');
+
+document.dispatchEvent(clickEvent);
