eb91e46af1f015a2c12c7e6a5ba9d0a430510e0b
Cory Wheeler <corywheeler@gmail.com>
2020-04-17 14:44:01 -0400

    Add in how things would have been done in the past.

--- experiments/rxjs-first-example-experiment.js
+++ experiments/rxjs-first-example-experiment.js
@@ -1,11 +1,25 @@
-// Code example came from RxJS - Overview page
-// https://rxjs-dev.firebaseapp.com/guide/overview
+/*
+ * Code example came from RxJS - Overview page
+ * https://rxjs-dev.firebaseapp.com/guide/overview
+ */
+
 
 import { fromEvent } from 'rxjs';
 
-let clickEvent = new window.Event('click');
+/*
+ * This is how you would have listened for an event without RxJS
+ */
+document.addEventListener('click', () => console.log('Old School Clicked!'));
 
-fromEvent(document, 'click').subscribe(() => console.log('Clicked!'));
+/*
+ * This is how you would listen for listen for events using an Observable with RxJS
+ */
+fromEvent(document, 'click').subscribe(() => console.log('RxJS Clicked!'));
+
+/*
+ * Now fire some events. You will see both happen.
+ */
+let clickEvent = new window.Event('click');
 
 for (let i = 0; i < 3; i++) {
 	document.dispatchEvent(clickEvent);
