a604ef673cbfdfbbe8e69d9765e9d6f327cff546
Cory Wheeler <corywheeler@gmail.com>
2020-04-19 20:53:29 -0400

    Add in code to isolate the traditional way from the RxJS way.

--- experiments/rxjs-first-example-experiment.js
+++ experiments/rxjs-first-example-experiment.js
@@ -7,20 +7,43 @@
 import { fromEvent } from 'rxjs';
 
 /*
+ * Set up a click handler for use in showing the traditional way of click handling.
+ */
+const oldSchoolClickedHandler = () => console.log('Old School Clicked!');
+
+/*
  * This is how you would have listened for an event without RxJS
  */
-document.addEventListener('click', () => console.log('Old School Clicked!'));
+document.addEventListener('click', oldSchoolClickedHandler);
 
 /*
- * This is how you would listen for listen for events using an Observable with RxJS
+ * Now fire some events
  */
-fromEvent(document, 'click').subscribe(() => console.log('RxJS Clicked!'));
-
-/*
- * Now fire some events. You will see both happen.
- */
-let clickEvent = new window.Event('click');
+const clickEvent = new window.Event('click');
 
 for (let i = 0; i < 3; i++) {
 	document.dispatchEvent(clickEvent);
 }
+
+/*
+ * Unbind the oldSchoolClickedHandler event listener so we can see the RxJS handler in isolation.
+ */
+document.removeEventListener('click', oldSchoolClickedHandler);
+
+/*
+ * This is how you would listen for listen for events using an Observable with RxJS
+ */
+const clickSubscription = fromEvent(document, 'click').subscribe(() => console.log('RxJS Clicked!'));
+
+/*
+ * Now fire some events.
+ */
+
+for (let i = 0; i < 3; i++) {
+	document.dispatchEvent(clickEvent);
+}
+
+/*
+ * Unsubscribe from the RxJS subscription for the click event.
+ */
+clickSubscription.unsubscribe();
