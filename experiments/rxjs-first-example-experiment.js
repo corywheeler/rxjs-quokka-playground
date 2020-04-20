974dfe49fb5e8e084c699f4fc7c5a6af029d6ade
Cory Wheeler <corywheeler@gmail.com>
2020-04-20 07:36:57 -0400

    Adjust comment styles

--- experiments/rxjs-first-example-experiment.js
+++ experiments/rxjs-first-example-experiment.js
@@ -3,31 +3,25 @@
  * https://rxjs-dev.firebaseapp.com/guide/overview
  */
 
-
 import { fromEvent } from 'rxjs';
-
-/*
- * Set up a click handler for use in showing the traditional way of click handling.
- */
-const oldSchoolClickedHandler = () => console.log('Old School Clicked!');
 
 /*
  * This is how you would have listened for an event without RxJS
  */
+
+// Set up a click handler for use in showing the traditional way of click handling.
+const oldSchoolClickedHandler = () => console.log('Old School Clicked!');
+
 document.addEventListener('click', oldSchoolClickedHandler);
 
-/*
- * Now fire some events
- */
+// Now fire some events
 const clickEvent = new window.Event('click');
 
 for (let i = 0; i < 3; i++) {
 	document.dispatchEvent(clickEvent);
 }
 
-/*
- * Unbind the oldSchoolClickedHandler event listener so we can see the RxJS handler in isolation.
- */
+// Unbind the oldSchoolClickedHandler event listener so we can see the RxJS handler in isolation.
 document.removeEventListener('click', oldSchoolClickedHandler);
 
 /*
@@ -35,15 +29,10 @@ document.removeEventListener('click', oldSchoolClickedHandler);
  */
 const clickSubscription = fromEvent(document, 'click').subscribe(() => console.log('RxJS Clicked!'));
 
-/*
- * Now fire some events.
- */
-
+// Now fire some events.
 for (let i = 0; i < 3; i++) {
 	document.dispatchEvent(clickEvent);
 }
 
-/*
- * Unsubscribe from the RxJS subscription for the click event.
- */
+// Unsubscribe from the RxJS subscription for the click event.
 clickSubscription.unsubscribe();
--- experiments/rxjs-purity-experiment.js
+++ experiments/rxjs-purity-experiment.js
@@ -5,30 +5,24 @@ import { fromEvent } from 'rxjs';
 import { scan } from 'rxjs/operators';
 
 /*
- * Set up a click handler for use in showing the traditional way of click handling.
- */
-const oldSchoolClickedHandler = () => console.log(`Old School Clicked ${++firstCount} times`);
-
-/*
  * Without RxJS you would create an impure function, where other pieces of your code can mess up your state.
  * Here, any other code that has scope to the firstCount variable would be able to change it.
  */
 
+// Set up a click handler for use in showing the traditional way of click handling.
+const oldSchoolClickedHandler = () => console.log(`Old School Clicked ${++firstCount} times`);
+
 let firstCount = 0;
 document.addEventListener('click', oldSchoolClickedHandler);
 
-/*
- * Now fire some events
- */
+// Now fire some events
 const clickEvent = new window.Event('click');
 
 for (let i = 0; i < 3; i++) {
 	document.dispatchEvent(clickEvent);
 }
 
-/*
- * Unbind the oldSchoolClickedHandler event listener so we can see the RxJS handler in isolation.
- */
+// Unbind the oldSchoolClickedHandler event listener so we can see the RxJS handler in isolation.
 document.removeEventListener('click', oldSchoolClickedHandler);
 
 
@@ -39,14 +33,10 @@ const clickSubscription = fromEvent(document, 'click')
 						.pipe(scan(count => count + 1, 0))
 						.subscribe(counter => console.log(`RxJS Clicked ${counter} times`));
 
-/*
- * Now fire some more events
- */
+// Now fire some more events
 for (let i = 0; i < 3; i++) {
 	document.dispatchEvent(clickEvent);
 }
 
-/*
- * Unsubscribe from the RxJS subscription for the click event.
- */
+// Unsubscribe from the RxJS subscription for the click event.
 clickSubscription.unsubscribe();
