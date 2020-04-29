e4e7efb2446bad01112219babaa776870c0d6f1b
Cory Wheeler <corywheeler@gmail.com>
2020-04-28 21:05:53 -0400

    fix bug, need to use parameter passed in.

--- experiments/lib/unsubscribeAfterDuration.js
+++ experiments/lib/unsubscribeAfterDuration.js
@@ -1,5 +1,5 @@
 export const unsubscribeAfterDuration = (milliseconds, subscriptions) => {
 	return setTimeout(() => {
 		subscriptions.forEach(subscription => subscription.unsubscribe());
-	}, 5000);
+	}, milliseconds);
 }
